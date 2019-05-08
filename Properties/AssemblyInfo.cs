208012044c55d78702d087cef29a1bb7eae9b06d
seyous <41056280+seyous@users.noreply.github.com>
2019-05-08 10:17:52 +0100

    Add project files.

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SAM")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SAM")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("03dcbf6d-da1b-4561-b62b-0defba7e89aa")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTest1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium.Appium.Windows;
+using OpenQA.Selenium.Remote;
+
+namespace SAM
+{
+    [TestClass]
+    public class UnitTest1
+    {
+
+        protected static WindowsDriver<WindowsElement> session;
+
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+
+            try
+            {
+                DesiredCapabilities appCapabilities = new DesiredCapabilities();
+
+                appCapabilities.SetCapability("app", @"C:\Program Files\QinetiQ\SAM V2.0\bin\SAM.exe");
+
+                session = new WindowsDriver<WindowsElement>(new Uri("http://127.0.0.1:4723"), appCapabilities);
+
+                var retry = session.FindElementByClassName("Retry");
+
+                retry.Click();
+            }
+            catch (Exception)
+            {
+
+                
+            }
+        }
+    }
+}
